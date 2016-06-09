9f44a468e45a5b2836f56531e5f4342a22f78310
dineshkota3 <dineshkota3@gmail.com>
2016-06-09 14:42:36 +0530

    trying js

---
+++ js/totest.js
@@ -0,0 +1,16 @@
+alert("begin");
+d = document.getElementById("dinesh");
+
+alert("next");
+
+d.addEventListener("mousedown", function(event){
+     
+     alert("asdf");
+     d.innerHTML = d.innerHTML + 'asfaweuyquioeyq euyu eyquiwe tyqwuy ';
+}, false);
+
+d.addEventListener("mouseup", function(upevent){	
+    alert("asdfasfadsf");
+     d.innerHTML = d.innerHTML + 'asfaweuyquioeyqeyquiwtyqwuy ';
+		
+}, false);
