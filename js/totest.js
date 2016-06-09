2233ec3aecd16596acc73aff8060b824112aba9b
dineshkota3 <dineshkota3@gmail.com>
2016-06-09 15:14:08 +0530

    trying js

--- js/totest.js
+++ js/totest.js
@@ -1,63 +1,60 @@
-var cars = '{
-  "carsid": [
-    {
-      "id": "1",
-      "name": "Lambhorgini",
-      "year": "2010",
-      "color": "red",
-      "weight": "400",
-      "height": "40",
-      "dealer": "Dinesh",
-      "number": "12345675"
-    },
-    {
-      "id": "2",
-      "name": "Audi",
-      "year": "2010",
-      "color": "red",
-      "weight": "400",
-      "height": "40",
-      "dealer": "Dinesh",
-      "number": "123345675"
-    },
-    {
-      "id": "3",
-      "name": "Skoda",
-      "year": "2012",
-      "color": "Black",
-      "weight": "500",
-      "height": "50",
-      "dealer": "Dinesh",
-      "number": "123345675"
-    },
-    {
-      "id": "4",
-      "name": "Skoda",
-      "year": "2014",
-      "color": "yellow",
-      "weight": "500",
-      "height": "50",
-      "dealer": "Dinesh",
-      "number": "123345675"
-    },
-    {
-      "id": "5",
-      "name": "Ferrari",
-      "year": "2014",
-      "color": "Black",
-      "weight": "600",
-      "height": "60",
-      "dealer": "Dinesh",
-      "number": "123345675);"
-    }
-  ]
-}';
+var carsid = [
+  {
+    "id": "1",
+    "name": "Lambhorgini",
+    "year": "2010",
+    "color": "red",
+    "weight": "400",
+    "height": "40",
+    "dealer": "Dinesh",
+    "number": "12345675"
+  },
+  {
+    "id": "2",
+    "name": "Audi",
+    "year": "2010",
+    "color": "red",
+    "weight": "400",
+    "height": "40",
+    "dealer": "Dinesh",
+    "number": "123345675"
+  },
+  {
+    "id": "3",
+    "name": "Skoda",
+    "year": "2012",
+    "color": "Black",
+    "weight": "500",
+    "height": "50",
+    "dealer": "Dinesh",
+    "number": "123345675"
+  },
+  {
+    "id": "4",
+    "name": "Skoda",
+    "year": "2014",
+    "color": "yellow",
+    "weight": "500",
+    "height": "50",
+    "dealer": "Dinesh",
+    "number": "123345675"
+  },
+  {
+    "id": "5",
+    "name": "Ferrari",
+    "year": "2014",
+    "color": "Black",
+    "weight": "600",
+    "height": "60",
+    "dealer": "Dinesh",
+    "number": "123345675);"
+  }
+];
 
-obj = JSON.parse(cars);
 
 alert("begin");
 d = document.getElementById("add_cars");
 
-alert(obj.count);
+alert(carsid.count);
 
 
